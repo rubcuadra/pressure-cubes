eaf13c519f5d670f778b160298828693b7f4949a
rubcuadra <rubcuadra@gmail.com>
2017-09-16 16:34:57 -0500

    adding font-awesome

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css'; //Cargar hoja de estilos
-
+import './styles/css/font-awesome.min.css';
 //middlewares
 import thunk from 'redux-thunk';
 import timerMiddleware from 'redux-timer-middleware';
