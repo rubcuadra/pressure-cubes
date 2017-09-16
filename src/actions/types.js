1e2394bb8320e8463d6c2cbf5bf0d2168d27fb9c
rubcuadra <rubcuadra@gmail.com>
2017-09-16 17:13:47 -0500

    reducer added

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,6 @@
 export const GAME_PAUSE = "GAME_PAUSE";
 export const GAME_CONTINUE = "GAME_CONTINUE";
 export const TIMER_TICKED = "TIMER_TICKED";
+export const INCREASE_HEARTHS = "INC_H";
+export const DECREASE_HEARTHS = "DEC_H";
+export const RESET_HEARTHS = "RESET_H";
--- src/reducers/hearthsReducer.js
+++ src/reducers/hearthsReducer.js
@@ -1,3 +1,15 @@
-export default (state=5,action)=>{
-	return state;
+import {INCREASE_HEARTHS,DECREASE_HEARTHS,RESET_HEARTHS} from '../actions/types';
+
+const maxHearts = 5;
+export default (state=maxHearts,action)=>{
+	switch(action.type){
+		case INCREASE_HEARTHS:
+			return state+1;
+		case DECREASE_HEARTHS:
+			return state-1;
+		case RESET_HEARTHS:
+			return maxHearts;
+		default:
+			return state;
+	}
 }
